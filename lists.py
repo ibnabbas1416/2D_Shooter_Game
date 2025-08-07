0125b80843edddf03c638ab712b691b018fe19bc
ibnabbas1416 <Abdullah14314deen@gmail.com>
2025-08-07 13:05:32 +0100

    learning lists in python

---
+++ lists.py
@@ -0,0 +1,34 @@
+courses = ["History", "Math", "Physics","CompSci"]
+  
+
+
+
+
+
+  
+#course_str = ' - '.join(courses)
+#new_list = course_str.split(' - ')
+
+#print(course_str)
+#print(new_list)
+#for index, course in enumerate(courses, start=1):
+    #print(index, course)
+
+#popped  = courses.pop()
+#nums = [1, 5, 2, 4, 3]
+#for item in courses:
+    #print(item)
+#print('Math' in courses)
+
+#print(courses.index("CompSci"))
+
+#sorted_courses = sorted(courses)
+
+#courses.sort(reverse=True)
+#nums.sort(reverse=True)
+#courses.reverse()
+#print(popped)
+#print(sorted_courses)
+#print(sum(nums))
+#print(min(nums))
+#print(max(nums))
