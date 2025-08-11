4b18289e0c7e2df1da5fa222d060751275955276
ibnabbas1416 <Abdullah14314deen@gmail.com>
2025-08-11 19:09:46 +0100

    Adding the birthdays.py file

---
+++ guestpicnic.py
@@ -0,0 +1,16 @@
+all_guests = {'Abdullah': {'apples': 5, 'dates': 12},
+              'Miqdad': {'ham sandwiches': 3, 'apples': 2},
+              'Ziyadah': {'cups': 3, 'apple pies': 1}}
+def total_brought(guests, item):
+    num_brought = 0
+    for k, v in guests.items():
+        num_brought = num_brought + v.get(item, 0)
+    return num_brought 
+     
+print('Number of things being brought:')
+print(' - Apples         ' + str(total_brought(all_guests, 'apples')))
+print(' - Cups           ' + str(total_brought(all_guests, 'cups')))
+print(' - Cakes          ' + str(total_brought(all_guests, 'cakes')))
+print(' - Ham Sandwiches ' + str(total_brought(all_guests, 'ham sandwiches')))
+print(' - Apple Pies     ' + str(total_brought(all_guests, 'apple pies')))
+
